4eb1783797bdb112b491e0079ed163992303f2b2
Yosi Zelensky <yosiz@sentinelone.com>
2023-05-23 12:22:58 +0300

    Added menu

--- bot/start.py
+++ bot/start.py
@@ -8,12 +8,85 @@ from telegram.ext import CallbackContext, CommandHandler, Dispatcher
 # Init logger
 logger = getLogger(__name__)
 
+from telegram.ext import CommandHandler, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+############################### Bot ############################################
+def start(update, context):
+  update.message.reply_text(main_menu_message(),
+                            reply_markup=main_menu_keyboard())
+
+def main_menu(update,context):
+  query = update.callback_query
+  query.answer()
+  query.edit_message_text(
+                        text=main_menu_message(),
+                        reply_markup=main_menu_keyboard())
+
+def first_menu(update,context):
+  query = update.callback_query
+  query.answer()
+  query.edit_message_text(
+                        text=first_menu_message(),
+                        reply_markup=first_menu_keyboard())
+
+def second_menu(update,context):
+  query = update.callback_query
+  query.answer()
+  query.edit_message_text(
+                        text=second_menu_message(),
+                        reply_markup=second_menu_keyboard())
+
+# and so on for every callback_data option
+def first_submenu(bot, update):
+  pass
+
+def second_submenu(bot, update):
+  pass
+
+############################ Keyboards #########################################
+def main_menu_keyboard():
+  keyboard = [[InlineKeyboardButton('Option 1', callback_data='m1')],
+              [InlineKeyboardButton('Option 2', callback_data='m2')],
+              [InlineKeyboardButton('Option 3', callback_data='m3')]]
+  return InlineKeyboardMarkup(keyboard)
+
+def first_menu_keyboard():
+  keyboard = [[InlineKeyboardButton('Submenu 1-1', callback_data='m1_1')],
+              [InlineKeyboardButton('Submenu 1-2', callback_data='m1_2')],
+              [InlineKeyboardButton('Main menu', callback_data='main')]]
+  return InlineKeyboardMarkup(keyboard)
+
+def second_menu_keyboard():
+  keyboard = [[InlineKeyboardButton('Submenu 2-1', callback_data='m2_1')],
+              [InlineKeyboardButton('Submenu 2-2', callback_data='m2_2')],
+              [InlineKeyboardButton('Main menu', callback_data='main')]]
+  return InlineKeyboardMarkup(keyboard)
+
+############################# Messages #########################################
+def main_menu_message():
+  return 'Choose the option in main menu:'
+
+def first_menu_message():
+  return 'Choose the submenu in first menu:'
+
+def second_menu_message():
+  return 'Choose the submenu in second menu:'
+
+############################# Handlers #########################################
 
 def init(dispatcher: Dispatcher):
     """Provide handlers initialization."""
-    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
+    dispatcher.add_handler(CallbackQueryHandler(first_menu, pattern='m1'))
+    dispatcher.add_handler(CallbackQueryHandler(second_menu, pattern='m2'))
+    dispatcher.add_handler(CallbackQueryHandler(first_submenu,
+                                                pattern='m1_1'))
+    dispatcher.add_handler(CallbackQueryHandler(second_submenu,
+                                                    pattern='m2_1'))
 
 
-def start(update: Update, _: CallbackContext):
-    """Process a /start command."""
-    update.message.reply_text(text="I'm a bot, please talk to me!")
+
+# def start(update: Update, _: CallbackContext):
+#     """Process a /start command."""
+#     update.message.reply_text(text="I'm a bot, please talk to me!")
