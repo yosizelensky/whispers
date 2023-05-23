d82fad5db07caf1b492764e63b9bb93c984780ce
Yosi Zelensky <yosiz@sentinelone.com>
2023-05-23 12:06:17 +0300

    Add API Token

--- configurations/settings.py
+++ configurations/settings.py
@@ -1,5 +1,5 @@
-TOKEN = "bot:Token"
-NAME = "seed_bot"
+TOKEN = "6090632640:AAFKTD7BYwF0KJLJLhbKQV7WZwxG98GBC68"
+NAME = "Whispers"
 WEBHOOK = False
 # The following configuration is only needed if you setted WEBHOOK to True #
 WEBHOOK_OPTIONS = {
